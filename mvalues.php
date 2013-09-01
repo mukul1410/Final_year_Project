dde7c64bca429b534696d22eb576f613873c37f5
Mukul Rawal <rawal.mukul@hotmail.com>
2013-09-01 20:12:38 +0530

    Create mvalues.php

---
+++ mvalues.php
@@ -0,0 +1,23 @@
+<?php 
+require_once("database_connection.php");
+$query="Select * from msg where rid='".$_COOKIE['uid']."' order by ntime desc Limit 10";
+//echo $query;
+$q2=mysql_query($query);
+$str="";
+while($row=mysql_fetch_array($q2))
+{
+	if($row['nread']==1)
+	$color='black';
+	else
+	$color='#0072C6';
+	$query2="Select * from student_entity where uid='".$row['sid']."'";
+	$q3=mysql_query($query2);
+	$row2=mysql_fetch_array($q3);
+	$temp="<div id=".$row['nid']." class='mvalue' style='color:black' name='".$row['sid']."'>
+	<div class='title' ><h3 style='font-weight:bold;color:$color'>".$row2['sname']."</h3></div>
+	<div class='content' style='color:$color';><p>".$row['nmsg']."</p></div>
+	</div>";
+	$str.=$temp;
+}
+	echo $str;
+?>
