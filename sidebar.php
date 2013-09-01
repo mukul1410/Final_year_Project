9308e0ca9d27cc267fd3e8e8aa013b59707012c3
Mukul Rawal <rawal.mukul@hotmail.com>
2013-09-01 20:13:40 +0530

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,67 @@
+<div id="sidebar">
+
+<div id="home" name="0">
+<div class="photo">
+<img src="images/home.png" height="30px"  width="30px" />
+</div>
+<div class="text">
+<p style="padding-left:5px">Home</p>
+</div>
+
+</div>
+
+<div id="profile" name="1">
+<div class="photo">
+<img src="images/profile.png" height="30px"  width="30px" />
+</div>
+<div class="text">
+<p style="padding-left:5px">Profile</p>
+</div>
+</div>
+
+<div id="academics" name="2">
+<div class="photo">
+<img src="images/academics.png" height="30px"  width="30px" />
+</div>
+<div class="text">
+<p style="padding-left:5px">Academics</p>
+</div>
+</div>
+
+<div id="messages" name="3">
+<div class="photo">
+<img src="images/message.png" height="30px"  width="30px" />
+</div>
+<div class="text">
+<p style="padding-left:5px">Messages</p>
+</div>
+</div>
+
+<div id="library" name="4">
+<div class="photo">
+<img src="images/library.png" height="30px"  width="30px" />
+</div>
+<div class="text">
+<p style="padding-left:5px">Library</p>
+</div>
+</div>
+
+<div id="books" name="5">
+<div class="photo">
+<img src="images/books.png" height="30px"  width="30px" />
+</div>
+<div class="text">
+<p style="padding-left:5px">Books</p>
+</div>
+</div>
+
+<div id="events" name="6">
+<div class="photo">
+<img src="images/events.png" height="30px"  width="30px" />
+</div>
+<div class="text">
+<p style="padding-left:5px">Events</p>
+</div>
+</div>
+
+</div>
